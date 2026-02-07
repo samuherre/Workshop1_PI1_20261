cc774eb9b70ad52252b90fc4738d57abe09a249f
Samuel Herrera Galvis <samuherre021@gmail.com>
2026-02-07 17:20:41 -0500

    Agregar peliculas a la BD

--- movie/models.py
+++ movie/models.py
@@ -4,6 +4,6 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=250)
+    description = models.CharField(max_length=500)
     image = models.ImageField(upload_to='movies/images/')
     url = models.URLField(blank=True)
